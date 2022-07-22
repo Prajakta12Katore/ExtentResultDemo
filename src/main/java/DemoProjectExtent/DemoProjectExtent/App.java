722ee5ca586597b3b2136a056c19918182e0c16c
prajaktaka <prajaktaka@prajaktaka-w10.cybage.com>
2022-07-22 12:55:10 +0530

    First Commit

---
+++ src/main/java/DemoProjectExtent/DemoProjectExtent/App.java
@@ -0,0 +1,13 @@
+package DemoProjectExtent.DemoProjectExtent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
